b93ba25d9594df984af98976af8828811ec4851f
Matheus Hisamoto de Souza <161658062+M-Hisamoto@users.noreply.github.com>
2024-04-30 17:17:45 -0300

    login

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function verificarIdade() {
+function verificarIdade() 
+{
     const inputIdade = document.getElementById('idade');
     const aviso = document.getElementById('aviso');
     const formularioIdade = document.getElementById('formularioIdade');
@@ -16,4 +17,26 @@ function verificarIdade() {
         aviso.textContent = '';
         formularioIdade.style.display = 'none';
     }
+}
+
+function Login() {
+    window.location.href="login.html";
+}
+
+function UsuarioSenha() {
+    const inputSenha = document.getElementById('senha');
+    const inputUsuario = document.getElementById('usuario');
+    const logar = document.getElementById('logar');
+
+    const usuario = inputUsuario.value;
+    const senha = inputSenha.value;
+
+    if (usuario === "1234" && senha === "1234") {
+        logar.textContent = 'Login concluído.'
+    } 
+    else {
+        logar.textContent = 'Usuário ou senha incorretos, por favor tente novamente.';
+    }
+
+    window.location.href="index.html";
 }
